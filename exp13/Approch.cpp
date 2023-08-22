4c0ef0f8d1d6ea8967f0435608806b662d657198
rakeshraja192121121 <142768391+rakeshraja192121121@users.noreply.github.com>
2023-08-22 08:43:39 +0530

    Add files via upload

---
+++ exp13/Approch.cpp
@@ -0,0 +1,26 @@
+#include "Approch.h"
+
+// Constructors/Destructors
+//  
+
+Approch::Approch () {
+initAttributes();
+}
+
+Approch::~Approch () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void Approch::initAttributes () {
+}
+
---
+++ exp13/BPO_MANAGEMENT_SYSTEM.cpp
@@ -0,0 +1,26 @@
+#include "BPO_MANAGEMENT_SYSTEM.h"
+
+// Constructors/Destructors
+//  
+
+BPO_MANAGEMENT_SYSTEM::BPO_MANAGEMENT_SYSTEM () {
+initAttributes();
+}
+
+BPO_MANAGEMENT_SYSTEM::~BPO_MANAGEMENT_SYSTEM () { }
+
+//  
+// Methods
+//  
+
+
+// Accessor methods
+//  
+
+
+// Other methods
+//  
+
+void BPO_MANAGEMENT_SYSTEM::initAttributes () {
+}
+
